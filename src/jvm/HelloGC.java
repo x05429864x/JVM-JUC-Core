426a11ee445416cbe02ec6a1e17f3be3feade853
xqr <x0542986x@sina.com>
2021-03-02 15:31:49 +0800

    vmstat.png

--- src/jvm/HelloGC.java
+++ src/jvm/HelloGC.java
@@ -10,8 +10,8 @@ package jvm;
  */
 public class HelloGC {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("****HelloGC");
-        byte[] ints = new byte[1024 * 1024 * 50];
-//        Thread.sleep(Integer.MAX_VALUE);
+//        System.out.println("****HelloGC");
+//        byte[] ints = new byte[1024 * 1024 * 50];
+        Thread.sleep(Integer.MAX_VALUE);
     }
 }
