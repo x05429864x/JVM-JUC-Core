2a7a0464406be0e8cda10138c84ac25d37b655f7
xqr <x0542986x@sina.com>
2021-03-03 13:37:45 +0800

    master

--- src/jvm/HelloGC.java
+++ src/jvm/HelloGC.java
@@ -12,6 +12,7 @@ public class HelloGC {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("****HelloGC");
         System.out.println(1111);
+        System.out.println("master");
 //        byte[] ints = new byte[1024 * 1024 * 50];
         Thread.sleep(Integer.MAX_VALUE);
     }
