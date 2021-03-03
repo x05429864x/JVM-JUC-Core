c3248f61a4415627da0ea9c136fff40382a3823d
xqr <x0542986x@sina.com>
2021-03-03 11:28:27 +0800

    test sout

--- src/jvm/HelloGC.java
+++ src/jvm/HelloGC.java
@@ -11,6 +11,7 @@ package jvm;
 public class HelloGC {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("****HelloGC");
+        System.out.println(1111);
 //        byte[] ints = new byte[1024 * 1024 * 50];
         Thread.sleep(Integer.MAX_VALUE);
     }
