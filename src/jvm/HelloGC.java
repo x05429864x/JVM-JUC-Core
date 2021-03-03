a350df630cc30cacbcf8db761ee5678bab4a596f
xqr <x0542986x@sina.com>
2021-03-03 11:18:47 +0800

    iostat.png

--- src/jvm/HelloGC.java
+++ src/jvm/HelloGC.java
@@ -10,7 +10,7 @@ package jvm;
  */
 public class HelloGC {
     public static void main(String[] args) throws InterruptedException {
-//        System.out.println("****HelloGC");
+        System.out.println("****HelloGC");
 //        byte[] ints = new byte[1024 * 1024 * 50];
         Thread.sleep(Integer.MAX_VALUE);
     }
